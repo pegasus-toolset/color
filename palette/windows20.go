5d91281b9b1d424709d39057d55853c00ce87045
Norman Rauschen <n.rauschen@rafisa.ch>
2021-04-15 13:53:04 +0200

    Add Microsoft Windows default 20-color palette

--- palette/windows20.go
+++ palette/windows20.go
@@ -2,37 +2,45 @@ package palette
 
 import "github.com/pegasus-toolset/color"
 
-// Windows16 represents the Microsoft Windows default 16-color palette.
-var Windows16 = []color.Color{
+// Windows20 represents the Microsoft Windows default 20-color palette.
+var Windows20 = []color.Color{
 	// Black
 	color.RGB{R: 0x0, G: 0x0, B: 0x0},
-	// Maroon
+	// Dark Red
 	color.RGB{R: 0x80, G: 0x0, B: 0x0},
-	// Green
+	// Dark Green
 	color.RGB{R: 0x0, G: 0x80, B: 0x0},
-	// Olive
+	// Dark Yellow
 	color.RGB{R: 0x80, G: 0x80, B: 0x0},
-	// Navy
+	// Dark Blue
 	color.RGB{R: 0x0, G: 0x0, B: 0x80},
-	// Purple
+	// Dark Magenta
 	color.RGB{R: 0x80, G: 0x0, B: 0x80},
-	// Teal
+	// Dark Cyan
 	color.RGB{R: 0x0, G: 0x80, B: 0x80},
-	// Silver
+	// Light Grey
 	color.RGB{R: 0xc0, G: 0xc0, B: 0xc0},
-	// Gray
+	// Money Green
+	color.RGB{R: 0xc0, G: 0xdc, B: 0xc0},
+	// Sky Blue
+	color.RGB{R: 0xa6, G: 0xca, B: 0xf0},
+	// Cream
+	color.RGB{R: 0xff, G: 0xfb, B: 0xf0},
+	// Medium Grey
+	color.RGB{R: 0xa0, G: 0xa0, B: 0xa4},
+	// Dark Grey
 	color.RGB{R: 0x80, G: 0x80, B: 0x80},
 	// Red
 	color.RGB{R: 0xff, G: 0x0, B: 0x0},
-	// Lime
+	// Green
 	color.RGB{R: 0x0, G: 0xff, B: 0x0},
 	// Yellow
 	color.RGB{R: 0xff, G: 0xff, B: 0x0},
 	// Blue
 	color.RGB{R: 0x0, G: 0x0, B: 0xff},
-	// Fuchsia
+	// Magenta
 	color.RGB{R: 0xff, G: 0x0, B: 0xff},
-	// Aqua
+	// Cyan
 	color.RGB{R: 0x0, G: 0xff, B: 0xff},
 	// White
 	color.RGB{R: 0xff, G: 0xff, B: 0xff},
