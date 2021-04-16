0a99db1dff43f8c8af1c63d15159d0586402e1e6
Acid147 <norman.rauschen@web.de>
2021-04-16 15:28:25 +0200

    Add palettes by Kerrie Lake

---
+++ palette/coldfiregb.go
@@ -0,0 +1,11 @@
+package palette
+
+import "github.com/pegasus-toolset/color"
+
+// ColdfireGB palette by Kerrie Lake.
+var ColdfireGB = color.Palette{
+	color.RGB{R: 0x46, G: 0x42, B: 0x5e},
+	color.RGB{R: 0x5b, G: 0x76, B: 0x8d},
+	color.RGB{R: 0xd1, G: 0x7c, B: 0x7c},
+	color.RGB{R: 0xf6, G: 0xc6, B: 0xa8},
+}
---
+++ palette/nymphgb.go
@@ -0,0 +1,11 @@
+package palette
+
+import "github.com/pegasus-toolset/color"
+
+// NymphGB palette by Kerrie Lake.
+var NymphGB = color.Palette{
+	color.RGB{R: 0x2c, G: 0x21, B: 0x37},
+	color.RGB{R: 0x44, G: 0x61, B: 0x76},
+	color.RGB{R: 0x3f, G: 0xac, B: 0x95},
+	color.RGB{R: 0xa1, G: 0xef, B: 0x8c},
+}
