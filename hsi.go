f6f4fcd33767146d492410f6b60219be1ee46d81
Norman Rauschen <n.rauschen@rafisa.ch>
2021-04-20 08:47:26 +0200

    Add comments

--- hsi.go
+++ hsi.go
@@ -48,11 +48,13 @@ func (hsi HSI) RGBA() (r, g, b, a uint32) {
 	return uint32((rFloat + m) * 0xffff), uint32((gFloat + m) * 0xffff), uint32((bFloat + m) * 0xffff), 0xffff
 }
 
+// DistanceTo calculates the distance to another color in the HSI color space.
 func (hsi HSI) DistanceTo(c color.Color) float64 {
 	other := HSIModel.Convert(c).(HSI)
 	return math.Sqrt(math.Pow(other.H/360-hsi.H/360, 2) + math.Pow(other.S-hsi.S, 2) + math.Pow(other.I-hsi.I, 2))
 }
 
+// HSIModel can convert any color to a HSI color.
 var HSIModel color.Model = color.ModelFunc(hsiModel)
 
 func hsiModel(c color.Color) color.Color {
--- hsv.go
+++ hsv.go
@@ -48,11 +48,13 @@ func (hsv HSV) RGBA() (r, g, b, a uint32) {
 	return uint32((rFloat + m) * 0xffff), uint32((gFloat + m) * 0xffff), uint32((bFloat + m) * 0xffff), 0xffff
 }
 
+// DistanceTo calculates the distance to another color in the HSV color space.
 func (hsv HSV) DistanceTo(c color.Color) float64 {
 	other := HSVModel.Convert(c).(HSV)
 	return math.Sqrt(math.Pow(other.H/360-hsv.H/360, 2) + math.Pow(other.S-hsv.S, 2) + math.Pow(other.V-hsv.V, 2))
 }
 
+// HSVModel can convert any color to a HSV color.
 var HSVModel color.Model = color.ModelFunc(hsvModel)
 
 func hsvModel(c color.Color) color.Color {
